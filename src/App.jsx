fee7b2ef5f4bc8de98d5056be608a88d05562030
isidora franchini <n.franchinic@gmail.com>
2024-11-03 16:24:41 -0300

    Pizzeria Mamma Mia

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from "./Components/Navbar/Navbar"
+import Home from "./Components/Home/Home"
+import Footer from "./Components/Footer/Footer"
+
+const App = () => {
+  return(
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer />
+    </>
+  )
+}
+export default App
